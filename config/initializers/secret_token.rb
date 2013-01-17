bad9049658593d95cd283e23ffebae98cc3fe85e
francis <francis@omer-VGN-SZ650N.(none)>
2013-01-16 21:25:37 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockSimulator::Application.config.secret_token = '08ab6c57d0662b1d348d183ae80a08c27812fbfb0cb311f2c163725506a601bf3e5840a32ab0cfd848cbfa818a03972198a36fc505860abf29ed36031692d0ae'
