07db2cb0207200c7b63f6de0a347d21f8782c280
Francis Wang <go717franciswang@hotmail.com>
2013-02-22 00:46:25 -0800

    I18n annuithy frequency

--- app/models/annuity_freq_type.rb
+++ app/models/annuity_freq_type.rb
@@ -1,3 +1,18 @@
 class AnnuityFreqType < ActiveRecord::Base
   attr_accessible :name
+
+  def localize_name
+    case self.name.to_sym
+    when :Daily
+      I18n.t 'simulation.funit.daily'
+    when :Weekly
+      I18n.t 'simulation.funit.weekly'
+    when :Monthly
+      I18n.t 'simulation.funit.monthly'
+    when :Quarterly
+      I18n.t 'simulation.funit.quarterly'
+    when :Yearly
+      I18n.t 'simulation.funit.yearly'
+    end
+  end
 end
