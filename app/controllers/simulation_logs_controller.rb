a03e1c8d24fddb117758ba1acbad34c454c23efb
Francis Wang <go717franciswang@hotmail.com>
2013-02-22 01:01:42 -0800

    Change to query all simulation logs

--- app/controllers/simulation_logs_controller.rb
+++ app/controllers/simulation_logs_controller.rb
@@ -2,6 +2,6 @@ class SimulationLogsController < ApplicationController
   def show
     @simulation = Simulation.find(params[:id])
     @simulation_logs = @simulation.simulation_logs.
-      where('event != ""').paginate(page: params[:page])
+      paginate(page: params[:page])
   end
 end
